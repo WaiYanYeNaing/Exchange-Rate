4d11c3fd2b0bf0287b36b20b4cf72198d03690c4
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2023-09-12 13:45:40 +0630

    Currency History Chart and Live Rate

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,20 +1,70 @@
-"use client"; 
+"use client";
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Datepicker } from "flowbite-react";
+import Chart from "react-apexcharts";
 
 const CurrencyExchange = () => {
-  // Define state variables with default values
   const [currencies, setCurrencies] = useState({});
-  const [rate, setRate] = useState(1);
-  const [fromCurrencyType, setFromCurrencyType] = useState("USD"); // Default currency type is set to "US"
-  const [toCurrencyType, setToCurrencyType] = useState("MMK"); // Default currency type is set to "FR"
-  const [fromCurrencyValue, setFromCurrencyValue] = useState(1); // Default currency value is set to "1"
-  const [toCurrencyValue, setToCurrencyValue] = useState(1); // Default currency value is set to "1"
-  const [selectedDate, setSelectedDate] = useState(null); // Default selected date is null
+  const [liveRates, setLiveRates] = useState({});
+  const [exchangeRate, setExchangeRate] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("MMK");
+  const [fromCurrencyAmount, setFromCurrencyAmount] = useState(1);
+  const [toCurrencyAmount, setToCurrencyAmount] = useState(1);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const dates = [
+    { x: new Date("2023-09-01"), y: 1000000 },
+    { x: new Date("2023-09-02"), y: 1200000 },
+    { x: new Date("2023-09-03"), y: 12870000 },
+    { x: new Date("2023-09-04"), y: 13870808 },
+  ];
+  const [chartData, setChartData] = useState({
+    series: [{ name: "", data: dates }],
+    options: {
+      chart: {
+        type: "area",
+        stacked: false,
+        height: 350,
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      markers: {
+        size: 0,
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          inverseColors: false,
+          opacityFrom: 0.5,
+          opacityTo: 0,
+          stops: [0, 90, 100],
+        },
+      },
+      yaxis: {
+        labels: {
+          formatter: (val) => (val / 1000000).toFixed(5),
+        },
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      tooltip: {
+        shared: false,
+        x: {
+          formatter: (val) =>
+            new Date(val).toISOString().split("T00:00:00.000Z"),
+        },
+        y: {
+          formatter: (val) => (val / 1000000).toFixed(4),
+        },
+      },
+    },
+  });
 
-  // Define an array of exchange values
   const exchangeValues = [1, 5, 10, 25, 50, 100, 500, 1000, 5000, 10000, 50000];
 
   useEffect(() => {
@@ -28,14 +78,37 @@ const CurrencyExchange = () => {
           const data = await response.json();
           setCurrencies(data.currencies);
         } else {
-          console.error("API request for currency list failed");
+          console.error("Failed to fetch currency list from API");
         }
       } catch (error) {
         console.error("An error occurred while fetching currency list:", error);
       }
     };
 
+    const fetchLiveRates = async () => {
+      try {
+        const response = await fetch(
+          "http://apilayer.net/live?access_key=9090e947b8f4f1cb464c441036e62085"
+        );
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setLiveRates(data.quotes);
+        } else {
+          console.error("Failed to fetch live exchange rates from API");
+        }
+      } catch (error) {
+        console.error(
+          "An error occurred while fetching live exchange rates:",
+          error
+        );
+      }
+    };
+
     fetchCurrencyList();
+    setTimeout(() => {
+      fetchLiveRates();
+    }, 2000);
   }, []);
 
   useEffect(() => {
@@ -43,16 +116,16 @@ const CurrencyExchange = () => {
       setTimeout(async () => {
         try {
           const response = await fetch(
-            `http://apilayer.net/live?access_key=9090e947b8f4f1cb464c441036e62085&currencies=${toCurrencyType}&source=${fromCurrencyType}&format=1`
+            `http://apilayer.net/live?access_key=9090e947b8f4f1cb464c441036e62085&date=2023-09-12&currencies=${toCurrency}&source=${fromCurrency}&format=1`
           );
 
           if (response.status === 200) {
             const data = await response.json();
             const [currency, rate] = Object.entries(data.quotes)[0];
-            setRate(rate);
-            setToCurrencyValue((fromCurrencyValue * rate).toFixed(5));
+            setExchangeRate(rate);
+            setToCurrencyAmount((fromCurrencyAmount * rate).toFixed(5));
           } else {
-            console.error("API request for exchange rate failed");
+            console.error("Failed to fetch exchange rate from API");
           }
         } catch (error) {
           console.error(
@@ -64,64 +137,48 @@ const CurrencyExchange = () => {
     };
 
     fetchExchangeRate();
-  }, [fromCurrencyType, toCurrencyType]);
+  }, [fromCurrency, toCurrency]);
 
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
 
+  const handleFromCurrencyAmountChange = (e) => {
+    setToCurrencyAmount((e.target.value * exchangeRate).toFixed(5));
+    setFromCurrencyAmount(e.target.value);
+  };
 
-  // Event handlers for changing state
-  const handleFromCurrencyType = (e) => {
-    alert("Access Restricted - Your current Subscription Plan does not support Source Currency Switching.")
-    // setFromCurrencyType(e.target.value);
+  const handleToCurrencyAmountChange = (e) => {
+    setFromCurrencyAmount((e.target.value / exchangeRate).toFixed(5));
+    setToCurrencyAmount(e.target.value);
   };
-  const handleToCurrencyType = (e) => {
-    setToCurrencyType(e.target.value);
-  };
-  const handleFromCurrencyValue = (e) => { 
-    setToCurrencyValue((e.target.value * rate).toFixed(5)); 
-    setFromCurrencyValue(e.target.value);
-  };
-  const handleToCurrencyValue = (e) => {
-    setFromCurrencyValue((e.target.value / rate).toFixed(5)); 
-    setToCurrencyValue(e.target.value);
-  };
-  const handleSelectedDate = (date) => {
-    console.log(date);
+
+  const handleSelectedDateChange = (date) => {
     setSelectedDate(date);
   };
-  const getCountry = (inputString) => {
-    // Convert the string to lowercase
-    const lowercaseString = inputString.toLowerCase();
 
-    // Remove the last letter
-    const modifiedString = lowercaseString.slice(0, -1);
-
-    return modifiedString;
+  const getCountry = (currencyCode) => {
+    const lowercaseCode = currencyCode.toLowerCase();
+    return lowercaseCode.slice(0, -1);
   };
 
   return (
-    <main className="flex flex-col p-4 sm:px-32">
-      {/* Header */}
-      {/* <h5 className="text-2xl mb-2 font-bold ml-8 tracking-tighttext-white">
-        {fromCurrencyType} {currencies[fromCurrencyType]} to {toCurrencyType}{" "}
-        {currencies[toCurrencyType]}
-      </h5> */}
-
-      {/* First row */}
+    <main className="flex flex-col p-4 sm:px-20">
       <div className="flex flex-col sm:flex-row sm:p-8 gap-4 sm:gap-4">
-        {/* Exchange rate */}
-        <div className="w-full sm:w-8/12">
+        {/* Currency Converter */}
+        <div className="w-full sm:w-7/12">
           <div className="flex justify-center items-center max-w-full h-[380px] p-6 border rounded-lg shadow bg-gray-800 border-gray-700">
             <div className="flex flex-col space-y-3 lg:space-y-10">
-              <h5 className="text-2xl mb-2 font-bold tracking-tighttext-white">
-                {fromCurrencyType} {currencies[fromCurrencyType]} to{" "}
-                {toCurrencyType} {currencies[toCurrencyType]}
+              <h5 className="text-2xl mb-2 font-bold tracking-tight text-white">
+                {fromCurrency} {currencies[fromCurrency]} to {toCurrency}{" "}
+                {currencies[toCurrency]}
               </h5>
               <div className="flex flex-wrap">
                 <div className="grow space-y-3 pt-3 w-full lg:w-[160px] xl:w-52">
                   <select
-                    id="countries"
-                    value={fromCurrencyType}
-                    onChange={handleFromCurrencyType}
+                    id="fromCurrency"
+                    value={fromCurrency}
+                    onChange={(e) => handleFromCurrencyChange(e)}
                     className="order text-sm rounded-md block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   >
                     {Object.keys(currencies).map((currencyCode) => (
@@ -133,9 +190,9 @@ const CurrencyExchange = () => {
 
                   <input
                     type="text"
-                    id="first_name"
-                    value={fromCurrencyValue}
-                    onChange={handleFromCurrencyValue}
+                    id="fromCurrencyAmount"
+                    value={fromCurrencyAmount}
+                    onChange={(e) => handleFromCurrencyAmountChange(e)}
                     className="border text-sm rounded-md block w-full p-2.5 bg-gray-900 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     required
                   />
@@ -153,9 +210,9 @@ const CurrencyExchange = () => {
 
                 <div className="grow space-y-3 pt-3 w-full lg:w-[160px] xl:w-52">
                   <select
-                    id="countries"
-                    value={toCurrencyType}
-                    onChange={handleToCurrencyType}
+                    id="toCurrency"
+                    value={toCurrency}
+                    onChange={(e) => handleToCurrencyChange(e)}
                     className="order text-sm rounded-md block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   >
                     {Object.keys(currencies).map((currencyCode) => (
@@ -167,9 +224,9 @@ const CurrencyExchange = () => {
 
                   <input
                     type="text"
-                    id="first_name"
-                    value={toCurrencyValue}
-                    onChange={handleToCurrencyValue}
+                    id="toCurrencyAmount"
+                    value={toCurrencyAmount}
+                    onChange={(e) => handleToCurrencyAmountChange(e)}
                     className="border text-sm rounded-md block w-full p-2.5 bg-gray-900 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     required
                   />
@@ -178,80 +235,52 @@ const CurrencyExchange = () => {
               <div>
                 <Datepicker
                   className="w-52"
-                  onChange={() => handleSelectedDate} // Call the handler when date changes
+                  onChange={(date) => handleSelectedDateChange(date)}
                 />
               </div>
             </div>
           </div>
         </div>
 
-        {/* Live rate */}
-        <div className="w-full sm:w-4/12">
+        {/* Currency History */}
+        <div className="w-full sm:w-5/12">
           <div className="max-w-full h-[380px] border rounded-lg shadow p-6 sm:p-8 bg-gray-800 border-gray-700">
-            <div className="flex items-center justify-between mb-4">
-              <h5 className="text-xl font-bold leading-none text-white">
-                Latest Customers
-              </h5>
-              <a
-                href="#"
-                className="text-sm font-medium hover:underline text-blue-500"
-              >
-                View all
-              </a>
-            </div>
-            <div className="flow-root">
-              <ul role="list" className="divide-y divide-gray-700">
-                {/* Repeat list items as needed */}
-                <li className="py-3 sm:py-4">
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-shrink-0"></div>
-                    <div className="flex-1 min-w-0">
-                      <p className="text-sm font-medium truncate text-white">
-                        Neil Sims
-                      </p>
-                      <p className="text-sm truncate text-gray-400">
-                        email@windster.com
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-white">
-                      $320
-                    </div>
-                  </div>
-                </li>
-                {/* Repeat other list items */}
-              </ul>
+            <div id="chart">
+              <Chart
+                options={chartData.options}
+                series={chartData.series}
+                type="area"
+                height={350}
+              />
             </div>
           </div>
         </div>
       </div>
 
-      {/* Second row */}
       <div className="flex flex-col sm:flex-row sm:p-8 gap-4 sm:gap-4 sm:mt-[-2.7rem] mt-[1rem]">
-        {/* Exchange rate list */}
+        {/* Exchange Rate */}
         <div className="w-full sm:w-4/12">
           <div className="max-w-full h-[380px] p-6 border rounded-lg shadow bg-gray-800 border-gray-700">
             <p className="mb-2 text-md font-bold tracking-tight text-white">
-              Exchange Rate from {toCurrencyType} to {fromCurrencyType}
+              Exchange Rate from {toCurrency} to {fromCurrency}
             </p>
 
             <div className="flex justify-between mb-2">
               <div className="w-5/12 flex items-center">
                 <img
                   src={`https://flagcdn.com/w40/${getCountry(
-                    fromCurrencyType
+                    fromCurrency
                   )}.png`}
                   className="w-6 h-4 inline-block align-middle mr-2"
                 />
-                <div>{fromCurrencyType}</div>
+                <div>{fromCurrency}</div>
               </div>
               <div className="w-5/12 flex items-center">
                 <img
-                  src={`https://flagcdn.com/w40/${getCountry(
-                    toCurrencyType
-                  )}.png`}
+                  src={`https://flagcdn.com/w40/${getCountry(toCurrency)}.png`}
                   className="w-6 h-4 inline-block align-middle mr-2"
                 />
-                <div>{toCurrencyType}</div>
+                <div>{toCurrency}</div>
               </div>
             </div>
 
@@ -266,7 +295,7 @@ const CurrencyExchange = () => {
               <div className="w-5/12">
                 {exchangeValues.map((value) => (
                   <div key={value} className="font-normal text-gray-400">
-                    {(value * rate).toFixed(3)}
+                    {(value * exchangeRate).toFixed(3)}
                   </div>
                 ))}
               </div>
@@ -274,30 +303,29 @@ const CurrencyExchange = () => {
           </div>
         </div>
 
+        {/* Exchange Rate Reverse */}
         <div className="w-full sm:w-4/12">
           <div className="max-w-full h-[380px] p-6 border rounded-lg shadow bg-gray-800 border-gray-700">
             <p className="mb-2 text-md font-bold tracking-tight text-white">
-              Exchange Rate from {fromCurrencyType} to {toCurrencyType}
+              Exchange Rate from {fromCurrency} to {toCurrency}
             </p>
 
             <div className="flex justify-between mb-2">
               <div className="w-5/12 flex items-center">
                 <img
-                  src={`https://flagcdn.com/w40/${getCountry(
-                    toCurrencyType
-                  )}.png`}
+                  src={`https://flagcdn.com/w40/${getCountry(toCurrency)}.png`}
                   className="w-6 h-4 inline-block align-middle mr-2"
                 />
-                <div>{toCurrencyType}</div>
+                <div>{toCurrency}</div>
               </div>
               <div className="w-5/12 flex items-center">
                 <img
                   src={`https://flagcdn.com/w40/${getCountry(
-                    fromCurrencyType
+                    fromCurrency
                   )}.png`}
                   className="w-6 h-4 inline-block align-middle mr-2"
                 />
-                <div>{fromCurrencyType}</div>
+                <div>{fromCurrency}</div>
               </div>
             </div>
 
@@ -312,7 +340,7 @@ const CurrencyExchange = () => {
               <div className="w-5/12">
                 {exchangeValues.map((value) => (
                   <div key={value} className="font-normal text-gray-400">
-                    {(value / rate).toFixed(5)}
+                    {(value / exchangeRate).toFixed(5)}
                   </div>
                 ))}
               </div>
@@ -320,41 +348,24 @@ const CurrencyExchange = () => {
           </div>
         </div>
 
-        {/* Bar chart */}
+        {/* Live Rate */}
         <div className="w-full sm:w-4/12">
           <div className="max-w-full h-[380px] border rounded-lg shadow p-6 sm:p-8 bg-gray-800 border-gray-700">
-            <div className="flex items-center justify-between mb-4">
-              <h5 className="text-xl font-bold leading-none text-white">
-                Latest Customers
-              </h5>
-              <a
-                href="#"
-                className="text-sm font-medium hover:underline text-blue-500"
-              >
-                View all
-              </a>
+            <p className="mb-2 text-md font-bold tracking-tight text-white">
+              Live Rates
+            </p>
+            <div className="flex">
+              <span className="w-1/2">Name</span>
+              <span className="w-1/2">Rate</span>
             </div>
-            <div className="flow-root">
-              <ul role="list" className="divide-y divide-gray-700">
-                {/* Repeat list items as needed */}
-                <li className="py-3 sm:py-4">
-                  <div className="flex items-center space-x-4">
-                    <div className="flex-shrink-0"></div>
-                    <div className="flex-1 min-w-0">
-                      <p className="text-sm font-medium truncate text-white">
-                        Neil Sims
-                      </p>
-                      <p className="text-sm truncate text-gray-400">
-                        email@windster.com
-                      </p>
-                    </div>
-                    <div className="inline-flex items-center text-base font-semibold text-white">
-                      $320
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
+            {Object.keys(liveRates).map((currencyCode) => (
+              <div className="flex text-sm text-gray-400">
+                <span className="w-1/2">{currencyCode}</span>
+                <span className="w-1/2">
+                  {liveRates[currencyCode].toFixed(3)}
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
